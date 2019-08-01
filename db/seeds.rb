27a1d015dbdf7347655724c54279f8c58a8a7194
Wadami Kazuma <s1711581@s.tsukuba.ac.jp>
2019-08-01 16:41:40 +0900

    画像ではないseedデータ変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 urls = ["https://iroha-shop.jp/images/items/gkn_8.jpg", "https://www.mag2.com/p/news/wp-content/uploads/2017/10/kaneda051-650x401.jpg", "https://diamond.jp/mwimgs/e/8/-/img_e8c99f6a83862774eb9e86581b61f402150301.jpg", "https://image.rakuten.co.jp/light-works/cabinet/itemdetail/detail_lwg307_03.jpg","https://d2l930y2yx77uc.cloudfront.net/production/uploads/images/9295228/picture_pc_8fa324a32b77ef1056e4cecf16d8fc55.jpg"]
 
+
 urls.each do |url|
   notes = Note.create(image_url: url)
 end
