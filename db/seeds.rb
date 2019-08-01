40a9489a2a3086801d6b783f425640b794e83883
Wadami Kazuma <s1711581@s.tsukuba.ac.jp>
2019-08-01 20:04:32 +0900

    seed.rbがherokuに反映されないので修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-urls = ["https://iroha-shop.jp/images/items/gkn_8.jpg", "https://www.mag2.com/p/news/wp-content/uploads/2017/10/kaneda051-650x401.jpg", "https://diamond.jp/mwimgs/e/8/-/img_e8c99f6a83862774eb9e86581b61f402150301.jpg", "https://image.rakuten.co.jp/light-works/cabinet/itemdetail/detail_lwg307_03.jpg","https://d2l930y2yx77uc.cloudfront.net/production/uploads/images/9295228/picture_pc_8fa324a32b77ef1056e4cecf16d8fc55.jpg"]
+urls = ["https://iroha-shop.jp/images/items/gkn_8.jpg",
+  "https://www.mag2.com/p/news/wp-content/uploads/2017/10/kaneda051-650x401.jpg",
+  "https://diamond.jp/mwimgs/e/8/-/img_e8c99f6a83862774eb9e86581b61f402150301.jpg",
+  "https://image.rakuten.co.jp/light-works/cabinet/itemdetail/detail_lwg307_03.jpg",
+  "https://d2l930y2yx77uc.cloudfront.net/production/uploads/images/9295228/picture_pc_8fa324a32b77ef1056e4cecf16d8fc55.jpg"
+]
 
 
 urls.each do |url|
