3367acededa2fef0add9bad6e0769498c11ecb0d
taira120 <taira1999120@gmail.com>
2019-08-02 15:21:58 +0900

    Noteを作成するAPIを作った

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,7 +2,22 @@ class NotesController < ApplicationController
   def show
     render json: Note.find(params[:id])
   end
+
   def index
     render json: Note.all
+  end
+
+  def create
+    note = Note.new(note_params)
+
+    if note.save
+      render json: note
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:image_url)
   end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,7 @@
 class Note < ApplicationRecord
+  mount_uploader :image_url, NoteUploader
   has_many :pages
 
   validates :subject_name, presence: true
+  validates :image_url, presence: true
 end
