ebe29bf5acc01c5c56fc3663cb46bd8f3721394d
taira120 <taira1999120@gmail.com>
2019-08-06 13:21:52 +0900

    エラー処理を共通化した/バリデーションエラーを返すようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  private
+
+  def handle_400(object)
+    handle_error(400, 'Bad Request', detail: object.errors.messages)
+  end
+
+  def handle_error(status, message, detail:)
+    render status: status, json: { status: status, message: message, detail: detail }
+  end
 end
