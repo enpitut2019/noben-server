7739b0778cfa66063a0c9c75e6d4b5ebe120ed57
wutchy <dp4dp7@outlook.jp>
2019-08-08 23:26:43 +0900

    pageとtagモデルのテストを作成した。（imageはまだ）

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 require 'rails_helper'
+require 'carrierwave/test/matchers'
 
 RSpec.describe Page, type: :model do
   before do
@@ -7,17 +8,7 @@ RSpec.describe Page, type: :model do
       subject_name: "情報数学"
     )
   end
-    it "is valid with a order and image" do
-    image_path = File.join(Rails.root, 'spec/fixtures/home.png')
-    @page = @note.pages.new(
-      order: 1,
-      image: Rack::Test::UploadedFile.new(image_path)
-    )
-    pending 'この先はよくわからないので後で修正する'
-    expect(@page).to be_valid
-  end
-
-  pending '同じ'
+    
   it "is invalid without a order" do
     @page = @note.pages.new(order: nil)
     expect(@page.valid?).to eq(false)
